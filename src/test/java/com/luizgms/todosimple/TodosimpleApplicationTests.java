ef3994f4ef238254c803ddb1fcab56b35a7cda17
Luizgms <luiz.mendes@sga.pucminas.br>
2025-07-17 11:04:16 -0300

    first commit

---
+++ src/test/java/com/luizgms/todosimple/TodosimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizgms.todosimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
